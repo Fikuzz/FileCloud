cc202d9910ffa6a4bfef999fd1a5994ee3e9d89d
Fikuz <frana@FIKUZ>
2025-08-19 23:38:07 +0300

    feat GetChild

--- FileCloud.Core/Abstractions/IFilesRepositories.cs
+++ FileCloud.Core/Abstractions/IFilesRepositories.cs
@@ -7,6 +7,7 @@ namespace FileCloud.Core.Abstractions
         Task<Guid> Create(Core.Models.File file);
         Task<Guid> Delete(Guid id);
         Task<List<Result<Core.Models.File>>> GetAll();
+        Task<List<Result<Core.Models.File>>> GetChild(Guid id);
         Task<Result<Core.Models.File>> Get(Guid id);
         Task<Guid> Rename(Guid id, string name);
         Task<Guid> Move(Guid id, string path, Guid folderId);
