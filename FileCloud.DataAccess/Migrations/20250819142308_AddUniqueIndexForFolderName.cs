273a14e2334f25b04131dfa9230f939fdf453ac6
Fikuz <frana@FIKUZ>
2025-08-19 23:35:57 +0300

    new migration

---
+++ FileCloud.DataAccess/Migrations/20250819142308_AddUniqueIndexForFolderName.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FileCloud.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueIndexForFolderName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Folders_ParentId",
+                table: "Folders");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Folders_ParentId_Name",
+                table: "Folders",
+                columns: new[] { "ParentId", "Name" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Folders_ParentId_Name",
+                table: "Folders");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Folders_ParentId",
+                table: "Folders",
+                column: "ParentId");
+        }
+    }
+}
