bcec88ff616a2c0cbbde8e97453cb8128ac49576
Fikuz <frana@FIKUZ>
2025-08-20 18:08:53 +0300

    doc

--- FileCloud.Core/Result.cs
+++ FileCloud.Core/Result.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace FileCloud.Core
 {
     public class Result<T>
     {
+        [MemberNotNullWhen(true, nameof(Value))]
+        [MemberNotNullWhen(false, nameof(Error))]
         public bool IsSuccess { get; }
         public string? Error { get; }
         public T? Value { get; }
