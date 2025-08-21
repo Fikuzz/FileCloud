9f5ff1ff416fcd18c86f226dd110c2adf86129a4
Fikuz <frana@FIKUZ>
2025-08-21 04:03:30 +0300

    add getFiles

--- FileCloud.Core/Abstractions/IFolderService.cs
+++ FileCloud.Core/Abstractions/IFolderService.cs
@@ -1,21 +1,16 @@
 ﻿using FileCloud.Core.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FileCloud.Core.Abstractions
 {
     public interface IFolderService
     {
         public Task<List<Result<Folder>>> GetAllFolders();
-        public Task<Result<List<Folder>>> GetChildFolder(Guid id);
+        public Task<Result<List<Folder>>> GetSubFolder(Guid id);
+        public Task<Result<List<Core.Models.File>>> GetFiles(Guid id);
         public Task<Result<Folder>> GetFolder(Guid id);
         public Task<Result<Guid>> CreateFolder(string name, Guid? parentId);
         public Task<Result<Guid>> RenameFolder(Guid id, string name);
-        public Task<Result<Guid>> MoveFolder(Guid id, Folder? parent);
+        public Task<Result<Guid>> MoveFolder(Guid id, Guid? parentId);
         public Task<Result<Folder>> DeleteFolder(Guid id);
     }
 }
